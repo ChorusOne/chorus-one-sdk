1a7344ef83141dbaf68a2ffb0acd4765ddfc7a51
Mateusz Kaczanowski <mateusz@chorus.one>
2025-03-28 21:43:02 +0100

    cactus cosmos signer

---
+++ packages/signer-cactus-cosmos/src/index.ts
@@ -0,0 +1,1 @@
+export { CactusCosmosSigner } from './signer'
---
+++ packages/signer-cactus-cosmos/src/types.d.ts
@@ -0,0 +1,22 @@
+// NOTE: CactusLink cosmos extension doesn't have typescript intefrace exposed.
+// This is the best effort "loose" type representation of the CactusLink cosmos extension.
+export interface Key {
+  name: string
+  algo: string
+  pubKey: string
+  address: string
+  bech32Address: string
+  ethereumAddress: string
+  isNanoLedger: boolean
+}
+
+export interface AminoSignResponse {
+  signed: any
+  signature: any
+}
+
+export interface CactusLinkCosmos {
+  getKey(chainId: string): Promise<Key>
+
+  signAmino(chainId: string, signer: string, signDoc: any): Promise<AminoSignResponse>
+}
