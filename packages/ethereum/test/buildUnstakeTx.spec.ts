bfbc02ec89d43497ac1fde30cf8d0bd642baa7d9
Felipe Pessina <fpessina@chorus.one>
2025-04-22 21:04:38 +0400

    Include test for getMaxUnstake

--- packages/ethereum/test/buildUnstakeTx.spec.ts
+++ packages/ethereum/test/buildUnstakeTx.spec.ts
@@ -1,6 +1,6 @@
 import { EthereumStaker } from '@chorus-one/ethereum'
-import { Hex, PublicClient, WalletClient, formatEther, parseEther } from 'viem'
-import { prepareTests, stake } from './lib/utils'
+import { Hex, PublicClient, WalletClient, erc20Abi, formatEther, parseEther } from 'viem'
+import { mint, prepareTests, stake } from './lib/utils'
 import { assert } from 'chai'
 
 const amountToStake = parseEther('5')
@@ -12,6 +12,7 @@ describe('EthereumStaker.buildUnstakeTx', () => {
   let walletClient: WalletClient
   let publicClient: PublicClient
   let staker: EthereumStaker
+  let osEthTokenAddress: Hex
 
   beforeEach(async () => {
     const setup = await prepareTests()
@@ -21,6 +22,7 @@ describe('EthereumStaker.buildUnstakeTx', () => {
     publicClient = setup.publicClient
     walletClient = setup.walletClient
     staker = setup.staker
+    osEthTokenAddress = setup.osEthTokenAddress
 
     await stake({
       delegatorAddress,
@@ -70,5 +72,91 @@ describe('EthereumStaker.buildUnstakeTx', () => {
       Number(parseEther(assetsAfterStaking) - amountToUnstake),
       1
     )
+  })
+
+  it('performs a full cycle of stake, mint, burn, and unstake max', async () => {
+    const { maxMint } = await staker.getMint({
+      delegatorAddress,
+      validatorAddress
+    })
+
+    const amountToMint = parseEther(maxMint)
+
+    await mint({
+      delegatorAddress,
+      validatorAddress,
+      amountToMint,
+      publicClient,
+      walletClient,
+      staker
+    })
+
+    const osEthBalance = await publicClient.readContract({
+      abi: erc20Abi,
+      address: osEthTokenAddress,
+      functionName: 'balanceOf',
+      args: [delegatorAddress]
+    })
+
+    const { balance: mintedBalance } = await staker.getMint({
+      delegatorAddress,
+      validatorAddress
+    })
+
+    const mintedBalanceBigInt = parseEther(mintedBalance)
+
+    const unstakeAmount = mintedBalanceBigInt > osEthBalance ? osEthBalance : mintedBalanceBigInt
+
+    const { tx: burnTx } = await staker.buildBurnTx({
+      delegatorAddress,
+      validatorAddress,
+      amount: formatEther(unstakeAmount)
+    })
+
+    const burnRequest = await walletClient.prepareTransactionRequest({
+      ...burnTx,
+      chain: undefined
+    })
+
+    const burnHash = await walletClient.sendTransaction({
+      ...burnRequest,
+      account: delegatorAddress
+    })
+
+    const burnReceipt = await publicClient.waitForTransactionReceipt({ hash: burnHash })
+    assert.equal(burnReceipt.status, 'success')
+
+    const osEthBalanceAfterBurn = await publicClient.readContract({
+      abi: erc20Abi,
+      address: osEthTokenAddress,
+      functionName: 'balanceOf',
+      args: [delegatorAddress]
+    })
+
+    assert.closeTo(Number(osEthBalanceAfterBurn), 0, 1)
+
+    const { maxUnstake: maxUnstakeAfterBurn } = await staker.getStake({
+      delegatorAddress,
+      validatorAddress
+    })
+
+    const { tx: unstakeTx } = await staker.buildUnstakeTx({
+      delegatorAddress,
+      validatorAddress,
+      amount: maxUnstakeAfterBurn
+    })
+
+    const unstakeRequest = await walletClient.prepareTransactionRequest({
+      ...unstakeTx,
+      chain: undefined
+    })
+
+    const unstakeHash = await walletClient.sendTransaction({
+      ...unstakeRequest,
+      account: delegatorAddress
+    })
+
+    const unstakeReceipt = await publicClient.waitForTransactionReceipt({ hash: unstakeHash })
+    assert.equal(unstakeReceipt.status, 'success')
   })
 })
