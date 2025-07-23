82a86541350aa7c324082b7e6b5b1586decca914
Maria Varvaroi <maria@chorus.one>
2025-07-23 11:38:30 +0100

    Refactor solana tracking

--- packages/solana/src/tx.ts
+++ packages/solana/src/tx.ts
@@ -1,6 +1,8 @@
 import BigNumber from 'bignumber.js'
 import { checkMaxDecimalPlaces } from '@chorus-one/utils'
 import { LAMPORTS_PER_SOL, PublicKey, TransactionInstruction } from '@solana/web3.js'
+import { utf8ToBytes } from '@noble/ciphers/utils'
+import { sha256 } from '@noble/hashes/sha2'
 
 export function macroToDenomAmount (
   amount: string, // in macro denom (e.g. ATOM)
@@ -57,19 +59,21 @@ export function getDenomMultiplier (denomMultiplier?: string): string {
   return BigNumber(LAMPORTS_PER_SOL).toString(10)
 }
 
-const MEMO_PROGRAM_ID: PublicKey = new PublicKey('MemoSq4gqABAXKb96qnH8TysNcWxMyWCqXgDLGmfcHr')
+export function trackingStringToPubkey (inputString: string): PublicKey {
+  const inputBytes = utf8ToBytes(inputString)
+  const hashed = sha256(inputBytes)
 
-export function createMemoInstruction (memo: string, signerPubkeys?: Array<PublicKey>): TransactionInstruction {
-  const keys =
-    signerPubkeys == null
-      ? []
-      : signerPubkeys.map(function (key) {
-          return { pubkey: key, isSigner: true, isWritable: false }
-        })
+  return new PublicKey(hashed)
+}
+
+const NOOP_PROGRAM_ID = new PublicKey('noopb9bkMVfRPU8AsbpTUg8AQkHtKwMYZiFUjNRtMmV')
+
+export const getTrackingInstruction = (referrer: string): TransactionInstruction => {
+  const trackingPublicKey = trackingStringToPubkey(referrer)
 
   return new TransactionInstruction({
-    keys,
-    programId: MEMO_PROGRAM_ID,
-    data: Buffer.from(memo, 'utf8')
+    keys: [{ pubkey: trackingPublicKey, isSigner: false, isWritable: false }],
+    programId: NOOP_PROGRAM_ID,
+    data: Buffer.alloc(0)
   })
 }
