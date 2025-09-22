0dcc0477ca54074d63d2916b1f17e04ba95b3e43
Maria Varvaroi <maria@chorus.one>
2025-09-22 17:36:14 +0100

    Update types

--- packages/ethereum/src/lib/types/nativeStaking.ts
+++ packages/ethereum/src/lib/types/nativeStaking.ts
@@ -1,45 +1,7 @@
 import { Hex } from 'viem'
 
-export interface ValidatorDepositData {
-  pubkey: string
-  withdrawal_credentials: string
-  amount: number
-  signature: string
-  deposit_message_root: string
-  deposit_data_root: string
-  fork_version: string
-  network_name: string
-  deposit_cli_version: string
-}
-
-export interface ValidatorExitMessage {
-  message: {
-    epoch: string
-    validator_index: string
-  }
-  signature: string
-}
-
 export type ValidatorStatus = 'created' | 'active' | 'exited'
-
-export interface Validator {
-  pubkey: string
-  status: ValidatorStatus
-  deposit_data: ValidatorDepositData
-  exit_message?: ValidatorExitMessage
-}
-
-export interface ValidatorBatch {
-  batch_id: string
-  withdrawal_address: Hex
-  fee_recipient: Hex
-  number_of_validators: number
-  network: string
-  status: 'pending' | 'ready' | 'failed'
-  validators: Validator[]
-  created_at: string
-  updated_at: string
-}
+export type BatchStatusEnum = 'created' | 'ready'
 
 export interface CreateBatchRequest {
   batch_id: string
@@ -54,6 +16,40 @@ export interface CreateBatchResponse {
   message: string
 }
 
-export interface BatchStatusResponse extends ValidatorBatch {
-  statusCode?: number
+export interface BatchDetailsDepositData {
+  amount: number
+  deposit_cli_version: string
+  deposit_data_root: string
+  deposit_message_root: string
+  fork_version: string
+  network_name: string
+  pubkey: string
+  signature: string
+  withdrawal_credentials: string
+}
+
+export interface BatchDetailsValidator {
+  deposit_data: BatchDetailsDepositData
+  status: ValidatorStatus
+}
+
+export interface BatchDetailsResponse {
+  validators: BatchDetailsValidator[]
+  status: BatchStatusEnum
+  created: string
+  is_compounding: boolean
+  deposit_gwei_per_validator: number
+  status_code?: number // Optional HTTP status code
+}
+
+export interface ListBatchItem {
+  batch_id: string
+  created: number
+  status: BatchStatusEnum
+  is_compounding: boolean
+  deposit_gwei_per_validator: number
+}
+
+export interface ListBatchesResponse {
+  requests: ListBatchItem[]
 }
