5be201108614045b11a6c9e577a4731cd159ac31
Maria Varvaroi <maria@chorus.one>
2025-09-30 14:38:17 +0100

    Add address validation patterns and consensus layer constants for Ethereum network

--- packages/ethereum/src/lib/types/ethereumConfigNetwork.ts
+++ packages/ethereum/src/lib/types/ethereumConfigNetwork.ts
@@ -7,3 +7,10 @@ export interface EthereumNetworkConfig {
   // chain name i.e. ethereum, hoodi
   network: Networks
 }
+// EIP-7251 Consensus layer constants (in Gwei)
+export const MIN_ACTIVATION_BALANCE_GWEI = 2 ** 5 * 10 ** 9 // 32 ETH = 32000000000 Gwei
+export const MAX_EFFECTIVE_BALANCE_ELECTRA_GWEI = 2 ** 11 * 10 ** 9 // 2048 ETH = 2048000000000 Gwei
+
+// Address validation patterns
+export const ADDRESS_REGEX = /^0x[0-9a-fA-F]{40}$/
+export const COMPOUNDING_ADDRESS_REGEX = /^(0x02[a-fA-F0-9]{62})$/
